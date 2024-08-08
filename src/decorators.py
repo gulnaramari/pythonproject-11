1153f1cb2cf96a65b34435abc7f90f1e8993bec1
gulnaramari <gulnaramari@yandex.ru>
2024-08-08 16:18:24 +0300

    init 11.2

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+def log(filename=""):
+    """This decorator creates Log information"""
+    def my_decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ok")
+                else:
+                    print(f"{func.__name__} ok")
+                return result
+            except Exception as e:
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
+                else:
+                    print(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
+
+        return wrapper
+
+    return my_decorator
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+my_function(1, 2)
