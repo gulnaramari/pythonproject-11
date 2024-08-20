c88a2e01578bb35d6f282c4625767bd9b727ce2a
gulnaramari <gulnaramari@yandex.ru>
2024-08-20 14:07:03 +0300

    12_1 (1 функция)

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import json
+
+def get_data_about_transactions(path_to_file:str) -> list:
+	"""Функция принимает путь до файла, а на выход дает список словарей с транзакциями"""
+	try:
+		with open(path_to_file, "r", encoding="utf-8") as file:
+			try:
+				json_data_transactions = json.load(file)
+				if json_data_transactions == []:
+					return []
+			except json.JSONDecodeError:
+				return []
+	except FileNotFoundError:
+		return []
+	return json_data_transactions
+
+
+if __name__="__main__":
+	path_to_file="..\\data\\operations.json"
+    json_data_transactions=get_data_about_transactions(path_to_file)
+print(json_data_transactions)
